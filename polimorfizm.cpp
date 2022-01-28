12d82f558aa28908e9e4db22695628959c30f045
Adam Chyła <adam@chyla.org>
2022-01-28 18:38:37 +0100

    diament

--- polimorfizm.cpp
+++ polimorfizm.cpp
@@ -3,6 +3,41 @@
 #include <fstream>
 #include <memory>
 
+
+/*
+ ITransmitter    Diagnoser    Diagnoser
+         \\     //           //
+         Transmitter    Receiver
+                  \\   //
+                   Radio
+*/
+/*
+ ITransmitter     Diagnoser
+          \\     /        \
+         Transmitter    Receiver
+                  \\   //
+                   Radio
+*/
+
+struct Diagnoser {
+    Diagnoser(const std::string &file_name)
+     : file_name(file_name) {
+    }
+
+    virtual void diagnose(const std::string &message) {
+        const auto line = message + "\n";
+        std::ofstream out(file_name.c_str(), std::ofstream::app);
+        out.write(line.c_str(), line.length());
+        out.close();
+    }
+
+    virtual ~Diagnoser() = default;
+
+private:
+    const std::string file_name;
+};
+
+
 struct ITransmitter {
     // czy moge zrobic implementacje funkcji (cialo funkcji) send() w klasie ITransmitter?
     virtual void send(const std::string &text) = 0;
@@ -10,13 +45,14 @@ struct ITransmitter {
 };
 
 
-struct Transmitter : ITransmitter {
-    Transmitter() {
+struct Transmitter : ITransmitter, virtual protected Diagnoser {
+    Transmitter() : Diagnoser("transmitter.txt") {
         std::cout << "[Transmitter] creating...\n";
     }
 
     void send(const std::string &text) override {
         std::cout << "[Transmitter] sending: " << text << '\n';
+        diagnose("sent: " + text);
     }
 
     ~Transmitter() {
@@ -24,13 +60,14 @@ struct Transmitter : ITransmitter {
     }
 };
 
-struct Receiver {
-    Receiver() {
+struct Receiver : virtual protected Diagnoser {
+    Receiver() : Diagnoser("receiver.txt") {
         std::cout << "[Receiver] creating...\n";
     }
 
     std::string receive() {
         std::string text = "Received text.";
+        diagnose("received: " + text);
         return text;
     }
 
@@ -41,11 +78,12 @@ struct Receiver {
 
 
 struct Radio : Transmitter, Receiver {
-    Radio() {
+    Radio() : Diagnoser("radio.txt") {
     }
 
     void send(const std::string &text) override {
         std::cout << "[Radio] sending...\n";
+        Transmitter::diagnose("radio is sending: " + text);
         constexpr int num_of_chars_to_send = 10;
         for (std::string::size_type i = 0;
              i < text.length();
@@ -65,6 +103,7 @@ struct Radio : Transmitter, Receiver {
 int main() {
     std::unique_ptr<Radio> radio = std::make_unique<Radio>();
     radio->send("Hello, hello, hello, hello.");
+    radio->receive();
 
     return 0;
 }
