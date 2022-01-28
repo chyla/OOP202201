b511a5a39029b9fbdfada5c783d81f2236e36427
Adam Chyła <adam@chyla.org>
2022-01-28 17:12:55 +0100

    zmiany1

--- wprowadzenie.cpp
+++ wprowadzenie.cpp
@@ -1,3 +1,53 @@
+#include <iostream>
+
+class NumbersContainer {
+public:
+    using size_type = int;
+    using value_type = int;
+
+    NumbersContainer(size_type max_size)
+     : max_size(max_size),
+       count(0),
+       numbers(new value_type[max_size]) {
+    }
+
+    /* remember of rule of three/five */
+    // ...
+
+    void add(value_type value) {
+        if (count >= max_size) {
+            // thorw
+        }
+        else {
+            numbers[count] = value;
+            count++;
+        }
+    }
+
+    void print_content() {
+        for (size_type i = 0; i < count; ++i) {
+            std::cout << "Content pos: " << i << " value: " << numbers[i] << '\n';
+        }
+    }
+
+    ~NumbersContainer() {
+        delete [] numbers;
+    }
+
+private:
+    size_type max_size;
+    size_type count;
+    value_type *numbers;
+};
+
 int main() {
+    NumbersContainer containerA(10);
+
+    containerA.add(1);
+    containerA.add(3);
+    containerA.add(2);
+
+    containerA.print_content();
+
     return 0;
 }
